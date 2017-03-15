b8cf10d3b828ed1fb5316fa20acae758396818f8
李晓光 <lxg@ziteng.cn>
2017-03-15 16:13:47 +0800

    修改认证的部分问题

--- app/src/main/java/com/tj/pxdl/carlease/MenuAct/AuthActivity.java
+++ app/src/main/java/com/tj/pxdl/carlease/MenuAct/AuthActivity.java
@@ -1,10 +1,13 @@
 package com.tj.pxdl.carlease.MenuAct;
 
 import android.content.Intent;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.jakewharton.rxbinding.widget.RxTextView;
 import com.tj.opensrc.selectphoto.SystemAlbumPickerActivity;
 import com.tj.pxdl.carlease.R;
 import com.tj.pxdl.carlease.base.BaseActivity;
@@ -14,6 +17,9 @@ import com.tj.pxdl.carlease.widget.OneKeyClearEditText;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import rx.Observable;
+import rx.functions.Action1;
+import rx.functions.Func2;
 
 public class AuthActivity extends BaseActivity {
     private final int IMAGE_TAG_DRIVE=1;
@@ -33,6 +39,7 @@ public class AuthActivity extends BaseActivity {
     @BindView(R.id.user_tipView) TextView userTipView;
     @BindView(R.id.paperPhoto) ImageView paperPhoto;
     @BindView(R.id.paper_tipView) TextView paperTipView;
+    @BindView(R.id.submitBtn) Button submitBtn;
 
     @Override
     public void onCreate() {
@@ -40,9 +47,25 @@ public class AuthActivity extends BaseActivity {
         ButterKnife.bind(this);
         title.setText("用户认证");
 
+        Observable<CharSequence> userObservable= RxTextView.textChanges(userEdit);
+        Observable<CharSequence> idObservable= RxTextView.textChanges(idEdit);
+        Observable.combineLatest(userObservable, idObservable, new Func2<CharSequence, CharSequence, Boolean>() {
+            @Override
+            public Boolean call(CharSequence charSequence, CharSequence charSequence2) {
+
+                return null;
+            }
+        }).subscribe(new Action1<Boolean>() {
+            @Override
+            public void call(Boolean aBoolean) {
+
+            }
+        });
+
+
     }
 
-    @OnClick({R.id.leftBtn, R.id.driveLayoutBtn, R.id.userLayoutBtn, R.id.paperLayoutBtn})
+    @OnClick({R.id.leftBtn, R.id.driveLayoutBtn, R.id.userLayoutBtn, R.id.paperLayoutBtn,R.id.submitBtn})
     public void onClickListener(View view) {
         switch (view.getId()) {
             case R.id.leftBtn:
@@ -63,6 +86,14 @@ public class AuthActivity extends BaseActivity {
                 Intent paperIntent = new Intent(this, SystemAlbumPickerActivity.class);
                 paperIntent.putExtra(SystemAlbumPickerActivity.key_appPath,"photo/test");
                 startActivityForResult(paperIntent, IMAGE_TAG_PAPER);
+                break;
+            case R.id.submitBtn:
+                if(TextUtils.isEmpty(DRIVE_IMAGE_PATH)||TextUtils.isEmpty(USER_IMAGE_PATH)
+                        ||TextUtils.isEmpty(PAPER_IMAGE_PATH)){
+                    showTips("请选择照片后上传");
+                }else{
+
+                }
                 break;
         }
     }
